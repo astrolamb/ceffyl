fc528fa7e25076ca2a414e581482b6b52441c67e
William Lamb <60004701+astrolamb@users.noreply.github.com>
2023-02-24 16:03:10 -0600

    bug

--- ceffyl/Sampler.py
+++ ceffyl/Sampler.py
@@ -442,8 +442,6 @@ def setup_sampler(ceffyl, outdir, logL, logp, resume=True, jump=True,
 
             if s.CP:  # visit GW signals x5 more often
                 [groups.extend(s.pmap) for ii in range(5)]
-            else:  # group irn by psr - assumes irn added first
-                groups.extend(s.psrparam_idxs)
 
     # sampler
     sampler = ptmcmc(ceffyl.ndim, logL, logp, cov,
