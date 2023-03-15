c45e89692bd02976c354fdc53355068fde0ea492
wglamb17 <wglamb17@gmail.com>
2023-03-15 14:20:18 -0500

    remove ascii decoding

--- ceffyl/ceffyl_gp.py
+++ ceffyl/ceffyl_gp.py
@@ -559,7 +559,7 @@ if __name__ == '__main__':
     hyperparams = []
     for hp in hp_names:
         h = parameter.Uniform(gp[0].par_dict[hp]['min'],
-                              gp[0].par_dict[hp]['max'])(hp.decode('ascii'))
+                              gp[0].par_dict[hp]['max'])(hp)
         hyperparams.append(h)
 
     test_constant = True  # test if parameter.constant function works
