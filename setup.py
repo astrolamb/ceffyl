8edd550fff8e095855dea1d24fd1c3d77ffe1722
William Lamb <60004701+astrolamb@users.noreply.github.com>
2023-12-13 09:54:06 +0100

    added postinstallcommand to remove tempo2 dependency

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup
-
+from setuptools.command.install import install
 from Cython.Build import cythonize
 import numpy
-
+import sys
 from pathlib import Path
+import subprocess
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        # Install enterprise without dependencies
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install'
+                               '--no-deps', 'enterprise-pulsar'])
+
+
 setup(
     name='ceffyl',
-    version='1.3',
+    version='1.30.2',
     description=('Software to rapidly and flexibly analyse Pulsar Timing ' +
                  'Array data via factorised likelihood methods (Lamb et al. 2023)'),
     author='William G. Lamb',
@@ -17,11 +28,9 @@ setup(
     packages=['ceffyl', 'ceffyl.bw'],
     ext_modules=cythonize("ceffyl/bw/cbandwidths.pyx", include_path=["ceffyl/bw/"]),
     zip_safe=False,
-    install_requires = [
+    install_requires=[
         "encor>=1.1.2",
         "Cython>=0.29.33,<1.0.0",
-        "enterprise_extensions>=2.4.2,<3.0.0",
-        "enterprise_pulsar>=3.3.3,<4.0.0",
         "h5py>=3.7.0,<4.0.0",
         "kalepy>=1.3,<2.0.0",
         "KDEpy>=1.1.0,<2.0.0",
@@ -31,9 +40,12 @@ setup(
         "PTMCMCSampler>=2.1.1,<3.0.0",
         "scipy>=1.10.0,<2.0.0",
     ],
-    extras_require={"GP" : "holodeck-gw>=1.0.0,<2.0.0"},
+    extras_require={"GP": "holodeck-gw>=1.0.0,<2.0.0"},
     include_dirs=[numpy.get_include()],
     package_data={'cbandwidths': ['*'], "": ["*.pyx"]},
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    cmdclass={
+        'install': PostInstallCommand,
+    },
 )
